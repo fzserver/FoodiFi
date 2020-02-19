ae876b0152793eefd7bd3012da35f72b53b5fa62
Parth Aggarwal <frazile.com@gmail.com>
2020-02-19 11:47:18 +0530

    FoodiFi fix: Bugs Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:foodifi/pages/config/configPage.dart';
@@ -19,7 +20,15 @@ void main() {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
   // * Get Shared Preference Instance for whole app
-  // Frazile.prefs = await SharedPreferences.getInstance();
+
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runApp(ConfigPage());
 }
